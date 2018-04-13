57a25ff7fb926669264ac62179684fcaf1f774c2
Willem Mouwen <willem@sqlr.nl>
2018-04-14 01:46:19 +0200

    Remove obsolete views

--- resources/views/registration/form-gewis.blade.php
+++
@@ -1,8 +0,0 @@
-<p>
-    The following button will redirect you to GEWIS where you are asked to log in. You will be redirected back here
-    afterwards.
-</p>
-
-<p class="text-center">
-    <a href="{{ route('gewis.login') }}" class="btn btn-primary btn-lg">Login at GEWIS-website</a>
-</p>
